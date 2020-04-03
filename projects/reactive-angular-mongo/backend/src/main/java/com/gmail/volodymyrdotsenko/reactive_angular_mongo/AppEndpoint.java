fa219cbb17cbeecb0f30326134fe45d07c5da945
volodymyrd <volodymyrdotsenko@gmail.com>
2020-04-04 01:44:04 +0200

    scaffolding reactive-angular-mongo

---
+++ projects/reactive-angular-mongo/backend/src/main/java/com/gmail/volodymyrdotsenko/reactive_angular_mongo/AppEndpoint.java
@@ -0,0 +1,13 @@
+package com.gmail.volodymyrdotsenko.reactive_angular_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEndpoint {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEndpoint.class, args);
+	}
+
+}
---
+++ projects/reactive-angular-mongo/backend/src/test/java/com/gmail/volodymyrdotsenko/reactive_angular_mongo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.volodymyrdotsenko.reactive_angular_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
