308e279dcb1d8b2d5bb1e2993105f54f13450562
volodymyrd <volodymyrdotsenko@gmail.com>
2020-04-07 00:06:39 +0200

    implement ng functions.

--- projects/reactive-angular-mongo/reservation-app/src/app/app.component.ts
+++ projects/reactive-angular-mongo/reservation-app/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reservation-app';
+
+  rooms: Room[];
+
+  ngOnInit() {
+    this.rooms = [
+      new Room("125", "125", "100"),
+      new Room("200", "200", "250")
+    ]
+  }
+}
+
+export class Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+
+  constructor(id: string, roomNumber: string, price: string) {
+    this.id = id;
+    this.roomNumber = roomNumber;
+    this.price = price;
+  }
 }
