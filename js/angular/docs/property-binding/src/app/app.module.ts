b3b35a18d1005c46b98d71237b7bb4ce40d4c707
volodymyrd <volodymyrdotsenko@gmail.com>
2020-04-09 00:21:09 +0200

    add Two-way binding demo.

---
+++ js/angular/docs/property-binding/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SizerComponent } from './sizer/sizer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SizerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
