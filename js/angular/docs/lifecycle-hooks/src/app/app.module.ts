55d62a3100ebff13dd8965cf89eb5de2593671be
volodymyrd <volodymyrdotsenko@gmail.com>
2020-04-11 00:39:55 +0200

    add lifecycle-hooks.

---
+++ js/angular/docs/lifecycle-hooks/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PeekABooComponent } from './peek-a-boo.component';
+import {PeekABooParentComponent} from './peek-a-boo-parent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PeekABooParentComponent,
+    PeekABooComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
